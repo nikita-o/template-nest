4fd6edffb6f69611a9ba258a14ce16857d4d9445
Nikita <nik.oschepkov@yandex.ru>
2022-09-26 10:45:00 +0300

    swagger

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,5 +1,11 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 export function middlewares(app: INestApplication): INestApplication {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      enableDebugMessages: true,
+    }),
+  );
   return app;
 }
--- src/swagger.ts
+++ src/swagger.ts
@@ -1,5 +1,14 @@
 import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function swagger(app: INestApplication): INestApplication {
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api_doc', app, document);
   return app;
 }
